3cd65348d15b9e80311811c58bf3217674fb32f9
feary <fearycxy@gmail.com>
2019-04-22 20:04:10 +0800

    polls done

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,9 @@ from django.template import loader
 from django.http import Http404
 # Create your views here.
 from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
 from .models import Choice, Question
 
 def index(request):
@@ -13,6 +16,22 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailVIew(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsVIew(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
@@ -20,20 +39,25 @@ def results(request, question_id):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-    	selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-    	return render(request, 'polls/detail.html',{
-    		'question': question,
-    		'error_message': "You didn't select a choice."
-    		})
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
     else:
-    	selected_choice.votes += 1;
-    	selected_choice.save()
-    	return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
 
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question.question_text})
+    return render(request, 'polls/detail.html', {'question': question})
